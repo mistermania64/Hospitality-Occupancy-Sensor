0faba73ab7e601d517d6bbe2cf0bff2b2e1520da
mistermania64 <81997874+mistermania64@users.noreply.github.com>
2023-04-07 05:40:27 -0400

    API Pinging Works! JSON Parsing not fleshed out

--- js/pingURL.js
+++ js/pingURL.js
@@ -1,46 +1,62 @@
 //Javascript Document
+// function pingURL()
+// {
+// 	var URL = $("#url").val();
+// 	var settings = 
+// 	{
+// 		//Define configurations for Mock API Ping
+// 		cache: false,
+// 		dataType: "jsonp",
+// 		async: false,
+// 		crossDomain: true,
+// 		url: URL,
+// 		method: "GET",
+
+// 		headers:
+// 		{
+// 			accept: "application/json",
+// 			"Access-Control-Allow-Origin": "*",
+// 		},
+
+// 		//Defines custom responses for specific status codes
+// 		statusCode: 
+// 		{
+// 			200: function (response)
+// 			{
+// 				console.log("Status 200: Page is down.");
+// 			},
+
+// 			400: function (response)
+// 			{
+// 				console.log("Status 400: Page is down.")
+// 			},
+
+// 			0: function (response)
+// 			{
+// 				console.log("Status 0: Page is down.");
+// 			},
+// 		},
+// 	};
+
+// 	//Sends request to API and observes response
+// 	$.ajax(settings).done(function (response)
+// 	{
+// 		console.log(response);
+// 	});
+// }
+
 function pingURL()
 {
-	var URL = $("#url").val();
-	var settings = 
-	{
-		//Define configurations for Mock API Ping
-		cache: false,
-		dataType: "jsonp",
-		async: false,
-		crossDomain: true,
-		url: URL,
-		method: "GET",
+	var urlInput = document.getElementById("urlInput");
+	var urlToPing = urlInput.value;
 
-		headers:
-		{
-			accept: "application/json",
-			"Access-Control-Allow-Origin": "*",
-		},
-
-		//Defines custom responses for specific status codes
-		statusCode: 
-		{
-			200: function (response)
-			{
-				console.log("Status 200: Page is down.");
-			},
-
-			400: function (response)
-			{
-				console.log("Status 400: Page is down.")
-			},
-
-			0: function (response)
-			{
-				console.log("Status 0: Page is down.");
-			},
-		},
-	};
-
-	//Sends request to API and observes response
-	$.ajax(settings).done(function (response)
-	{
-		console.log(response);
-	});
+	fetch(`${urlToPing}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
 }
