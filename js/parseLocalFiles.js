37029a8077146e04aa66d2081b322babacb37969
mistermania64 <81997874+mistermania64@users.noreply.github.com>
2023-04-16 20:49:17 -0400

    Local parse now displays on screen

--- js/parseLocalFiles.js
+++ js/parseLocalFiles.js
@@ -2,5 +2,15 @@ function parseLocalFile(filename)
 {
    fetch(`${filename}`)
 	.then(response => response.json())
-	.then(data => console.log(data));
+	.then(data => {
+
+	console.log(data);
+
+	const json = JSON.stringify(data, null, 2); // Convert the data to a nicely formatted JSON string)
+	console.log(json);
+
+	document.getElementById('jsonResponseField').value = "JSON Response:\n" + json;
+	})
+
+  .catch(error => console.error('Error:', error));
 }
