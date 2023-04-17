25fd47475da6fa515c1c796dde1823defef75e3a
mistermania64 <81997874+mistermania64@users.noreply.github.com>
2023-04-17 02:43:56 -0400

    Local Parse Temporary JS Files

---
+++ js/equalSplitJSON.js
@@ -0,0 +1,22 @@
+function equalSplitJSON()
+{
+	var jsonObj = '{"ttlOccupants": 300,"occupied": 150,"vacant": 150}';
+
+   	var obj = JSON.parse(jsonObj);
+
+	//Extract specific values 
+	var occupied = obj.occupied;
+	var vacant = obj.vacant;
+	var ttlOccupants = obj.ttlOccupants;
+
+	//Update chart
+	setOccupants(ttlOccupants);
+	setOccupied(occupied);
+	setVacant(vacant);
+
+	const json = JSON.stringify(data, null, 2); // Convert the data to a nicely formatted JSON string)
+	console.log(json);
+
+	document.getElementById('jsonResponseField').value = "JSON Response:\n" + json;
+	
+}
---
+++ js/moreOccupiedJSON.js
@@ -0,0 +1,22 @@
+function moreOccupiedJSON()
+{
+	var jsonObj = '{"ttlOccupants": 300,"occupied": 275,"vacant": 25}';
+
+   var obj = JSON.parse(jsonObj);
+
+	//Extract specific values 
+	var occupied = obj.occupied;
+	var vacant = obj.vacant;
+	var ttlOccupants = obj.ttlOccupants;
+
+	//Update chart
+	setOccupants(ttlOccupants);
+	setOccupied(occupied);
+	setVacant(vacant);
+
+	const json = JSON.stringify(jsonObj, null, 2); // Convert the data to a nicely formatted JSON string)
+	console.log(json);
+
+	document.getElementById('jsonResponseField').value = "JSON Response:\n" + json;
+	
+}
