f85a18d44a04f46ec667a33be882b4d8be6495da
mistermania64 <81997874+mistermania64@users.noreply.github.com>
2023-04-25 01:12:00 -0400

    json5

--- js/parseLocalFile.js
+++ js/parseLocalFile.js
@@ -15,9 +15,46 @@ function parseLocalFile(filename)
 	const jsonObj = JSON.parse(data);
 
 	//Extract specific values from JSON object
-	newOccupied = parseInt(data.occupied);
-	newVacant = parseInt(data.vacant);
-	newRooms = parseInt(data.ttlRooms);
+	newOccupied = parseInt(jsonObj.occupied);
+	newVacant = parseInt(jsonObj.vacant);
+	newRooms = parseInt(jsonObj.ttlRooms);
+
+	//Update chart
+	setRooms(newRooms);
+	setOccupied(newOccupied);
+	setVacant(newVacant);
+
+	//Update Labels
+   updateLabels();
+
+	})
+
+  .catch(error => {
+  	console.error(error)
+  });
+
+}
+
+function parseLocalFile2(filename)
+{
+   fetch(`${filename}`)
+	.then(response => response.json())
+	.then(data => {
+
+	console.log(data);
+
+	//Stringify, log string, and print in textarea
+	const jsonString = JSON.stringify(data, null, 2); // Convert the data to a nicely formatted JSON string)
+	console.log(jsonString);
+	document.getElementById('jsonResponseField').value = "JSON Response:\n" + jsonString;
+
+	// Parse the returned data into a JSON object
+	const jsonObj = JSON.parse(data);
+
+	//Extract specific values from JSON object
+	newOccupied = parseInt(jsonObj.occupied);
+	newVacant = parseInt(jsonObj.vacant);
+	newRooms = parseInt(jsonObj.ttlRooms);
 
 	//Update chart
 	setRooms(newRooms);
